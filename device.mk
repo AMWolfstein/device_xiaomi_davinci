80bd3a4dd621c8e7400018ccfb08055c39532e2b
Jakub Pawlowski <jpawlowski@google.com>
2021-04-13 11:00:12 +0200

    davinci: Use bluetooth.audio@2.1
    bluetooth.audio@2.1 adds new functionalities required for Bluetooth LE
    Audio development
    
    Bug: 150670922
    Change-Id: Iac2ece0c029a38fef82e81f5ba3ba491163818ef

--- device.mk
+++ device.mk
@@ -43,7 +43,7 @@ PRODUCT_PACKAGES += \
     android.hardware.audio@7.1-util \
     android.hardware.audio.effect@7.0-impl \
     android.hardware.audio.service \
-    android.hardware.bluetooth.audio@2.0-impl
+    android.hardware.bluetooth.audio@2.1-impl
 
 PRODUCT_PACKAGES += \
     audio.bluetooth.default \
