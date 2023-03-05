1bdf67939ade3072de94cea93098775d3e5d4f54
AMWolfstein <ahmedhero4040@gmail.com>
2023-03-05 11:23:18 +0200

    davinci: AncientOS bringup

--- ancient_davinci.mk
+++ ancient_davinci.mk
@@ -11,10 +11,17 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 # Inherit from davinci device
 $(call inherit-product, device/xiaomi/davinci/device.mk)
 
-# Inherit some common Lineage stuff.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+# Inherit some common AncientOS stuff.
+$(call inherit-product, vendor/ancient/config/common_full_phone.mk)
 
-PRODUCT_NAME := lineage_davinci
+EXTRA_UDFPS_ANIMATIONS := true
+TARGET_BOOT_ANIMATION_RES := 1080
+TARGET_SUPPORTS_QUICK_TAP := true
+TARGET_SUPPORTS_CALL_RECORDING := true
+ANCIENT_OFFICIAL=true
+ANCIENT_GAPPS=true
+
+PRODUCT_NAME := ancient_davinci
 PRODUCT_DEVICE := davinci
 PRODUCT_BRAND := Xiaomi
 PRODUCT_MODEL := Mi 9T
